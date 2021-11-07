6ba281cc17e5e75f7da1eb6408b9948322a72a28
LeTheLu <48428230+LeTheLu@users.noreply.github.com>
2021-11-07 20:49:01 +0700

    thay doi

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -14,79 +14,121 @@ class _TodoPageState extends State<TodoPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8),
-          child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  flex:1,
-                    child: Container(
-                      height: 100,
-                      width: MediaQuery.of(context).size.width -20,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[300],
-                        borderRadius: BorderRadius.all(Radius.circular(30))
-                      ),
-                  child: Center(
-                    child: Text(widget.todo.title,style: TextStyle(color: Colors.teal, fontWeight: FontWeight.bold, fontSize: 30),
-                  ),),
-                )),
-                const SizedBox(height: 20),
-                Expanded(
-                  flex: 8,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(15)
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8),
-                      child: SingleChildScrollView(
-                        child: TextField(
-                          decoration: InputDecoration.collapsed(hintText: "Text"),
-                        ),
-                      ),
-                    ),
-                  ),),
-                Expanded(
-                    flex: 3,
-                    child: Container(
-                      child: Column(
-                        children: [
-                          SizedBox(height: 20,),
-                          Container(
-                            height: 60,
-                            width: MediaQuery.of(context).size.width - 50,
-                            decoration: BoxDecoration(
-                              color: Colors.teal,
-                              borderRadius: BorderRadius.all(Radius.circular(15)),
-                            ),
-                            child: Center(child: Text(widget.todo.datetime!.day.toString())),
-                          ),
-                          SizedBox(height: 20,),
-                          Container(
-                              height: 50,
-                              width: 100,
-                              child: RaisedButton(
-                                shape: const RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.all(Radius.circular(15))
-                                ),
-                                padding: EdgeInsets.all(10),
-                                onPressed: (){
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex:1,
+                        child: GestureDetector(
 
-                                },
+                          onTap: (){
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) => _buildPopupDialog(context, widget.todo.title));
+                          },
+
+                          child: Container(
+                            height: 100,
+                            width: MediaQuery.of(context).size.width -20,
+                            decoration: BoxDecoration(
+                              color: Colors.grey[300],
+                              borderRadius: const BorderRadius.all(Radius.circular(30))
+                            ),
+                      child: Center(
+                          child: Text(widget.todo.title,style: const TextStyle(color: Colors.teal, fontWeight: FontWeight.bold, fontSize: 30),
+                          ),
+                    )),
+                        )),
+                    const SizedBox(height: 20),
+                    Expanded(
+                      flex: 8,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(15)
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8),
+                          child: SingleChildScrollView(
+                            child: TextField(
+                              decoration: InputDecoration.collapsed(hintText: "Text"),
+                            ),
+                          ),
+                        ),
+                      ),),
+                    Expanded(
+                        flex: 3,
+                        child: Column(
+                          children: [
+                            const SizedBox(height: 20,),
+                            Container(
+                              height: 60,
+                              width: MediaQuery.of(context).size.width - 50,
+                              decoration: const BoxDecoration(
                                 color: Colors.teal,
-                                child: const Text("Save",style: TextStyle(color: Colors.white),),
-                              )),
-                        ],
-                      ),
-                    ))
-              ],
+                                borderRadius: BorderRadius.all(Radius.circular(15)),
+                              ),
+                              child: Center(child: Text(widget.todo.datetime!.day.toString())),
+                            ),
+                            const SizedBox(height: 20,),
+                            SizedBox(
+                                height: 50,
+                                width: 100,
+                                child: RaisedButton(
+                                  shape: const RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(Radius.circular(15))
+                                  ),
+                                  padding: const EdgeInsets.all(10),
+                                  onPressed: (){
+
+                                  },
+                                  color: Colors.teal,
+                                  child: const Text("Save",style: TextStyle(color: Colors.white),),
+                                )),
+                          ],
+                        ))
+                  ],
+                ),
             ),
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _buildPopupDialog(BuildContext context, String title) {
+    TextEditingController _controller  = TextEditingController();
+    _controller.text = title;
+    return AlertDialog(
+        content:  Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+
+              ),
+            )
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            textColor: Theme.of(context).primaryColor,
+            child: const Text('Save'),
+          ),
+        ],
     );
   }
 }
